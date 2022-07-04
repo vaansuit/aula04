c569c7c439e80c97173c579f98c2a84c4ccac5f5
Victor Hugo Vansuit <Victor.Vansuit@proway.treina>
2022-07-04 16:23:13 -0300

    [fixed]

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,108 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-  )); //espera um widget materialapp
-}
+// // void main() {
+// //   runApp(MaterialApp(
+// //     home: HomePage(),
+// //     debugShowCheckedModeBanner: false,
+// //   )); //espera um widget materialapp
+// // }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Center(child: Text('Olá mundo')),
-    );
-  }
-}
+// // class HomePage extends StatelessWidget {
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(),
+// //       body: const Center(child: Text('Olá mundo')),
+// //     );
+// //   }
+// // }
+
+// // void main() {
+// //   runApp(MaterialApp(
+// //     home: HomePage(),
+// //     debugShowCheckedModeBanner: false,
+// //   ));
+// // }
+
+// // class HomePage extends StatelessWidget {
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //         appBar: AppBar(),
+// //         body: Center(
+// //             child: Column(
+// //           children: [
+// //             const Text('Texto1'),
+// //             const Text('Texto2'),
+// //             Image.asset('invoker.jpeg'),
+// //           ],
+// //         )));
+// //   }
+// // }
+
+// void main() {
+//   runApp(MaterialApp(
+//     home: HomePage(),
+//     debugShowCheckedModeBanner: false,
+//   ));
+// }
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Center(
+//         child: Column(
+//           children: [
+//             const Text('Texto 1'),
+//             const Text('Texto 2'),
+//             SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: () {
+//                 print('Fui Clicado');
+//               },
+//               child: const Text('Push'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(),
+//         body: Center(
+//           child: Column(children: [
+//             Text('Texto 1'),
+//             Text('\n Texto 2 \n'),
+//             Image.asset('assets/img/invoker.jpeg'),
+//             ElevatedButton(
+//                 onPressed: () {
+//                   Image.asset('assets/img/pudge.jpeg');
+//                 },
+//                 child: const Text('Clique')),
+//           ]),
+//         ));
+//   }
+// }
+
+// class MyWidget extends StatelessWidget {
+//   @override
+
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: [
+//         Text('Hello'),
+//         Text('Hello'),
+//         Text('Hello'),
+//       ],
+//     );
+//   }
+// }
